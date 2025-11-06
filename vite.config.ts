b14d34e35b047e4754a401bde3b97bb1a75251c8
Serbastian Espitia <s.spitia@hotmail.com>
2025-11-06 12:42:01 +0100

    fix: update base path in vite.config.ts to root for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     // mode === 'development' &&
     // componentTagger(),
   ].filter(Boolean),
-  base: '/play-portfolio-playground/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
